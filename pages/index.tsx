bfd50ce36a9b6f7fbbae666a702bef25768b1d2d
Paul Nguon <herropaul@gmail.com>
2022-06-07 20:51:37 -0700

    UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className=' text-3xl font-bold underline'>
-      Hello World
+    <div className=' h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-xl text-center'>Which Artist is more popular?</div>
+      <div className=' p-3'/>
+      <div className=' border border-black rounded p-8 flex justify-between max-w-2xl items-center'>
+        <div className='h-32 w-32 rounded-full bg-green-900'></div>
+        <div className='px-10'>Or</div>
+        <div className='h-32 w-32 rounded-full bg-green-900'></div>
+      </div>
+      
     </div>
   )
 }
